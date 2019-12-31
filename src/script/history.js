0d485c3809236ad4a3b026455c38e98ff93053b1
wangguji <tianlang1997@126.com>
2019-12-31 17:21:42 +0800

    历史记录修改，待完善

--- src/script/history.js
+++ src/script/history.js
@@ -6,28 +6,68 @@ const path = require('path')
 const configDir = remote.app.getPath('userData')
 let historyList = []
 const historyDir = path.join(configDir, Config.getHistoryDir())
-// const DataBase = 'data'
+const TempHistoryFile = path.join(historyDir, 'temp')
+const History = 'record'
+const IndexFile = path.join(historyDir, 'index')
+let IndexArr
 // const IndexBase = 'index'
-let lastTime = moment().format('YYMMDD')
-let dataStore = {}
 export function appendHistory (record) {
-  if (!fs.existsSync(historyDir)) {
-    fs.mkdirSync(historyDir)
-  }
-  if (lastTime !== moment().format('YYMMDD')) {
-    flushHistory()
-  }
-  dataStore[record] = Date.now()
+  historyList.push(JSON.stringify([record, Date.now()]))
 }
 
 export function flushHistory () {
   if (historyList.length === 0) return
-  let fileName = moment().format('YYYY-MM-DD')
-  let content = historyList.join('')
+  let content = historyList.join('\n')
   historyList = []
-  fs.appendFileSync(path.join(historyDir, fileName), content)
+  fs.appendFileSync(path.join(historyDir, TempHistoryFile), content)
+}
+
+export function storeIndex () {
+  let record = moment().format('YYYY-MM')
+  getIndexFile()
+  if (IndexArr) {
+    if (IndexArr[IndexArr.length - 1] !== record) {
+      fs.appendFileSync(IndexFile, record + '\n')
+      IndexArr.push(record)
+    }
+  } else {
+    fs.appendFileSync(IndexFile, record + '\n')
+    IndexArr = [record]
+  }
+}
+
+export function getIndexFile () {
+  if (IndexArr) {
+    if (fs.existsSync(IndexFile)) {
+      let content = fs.readFile(IndexFile)
+      IndexArr = content.toString().split('\n')
+      return IndexArr
+    }
+  } else {
+    return []
+  }
+}
+
+function _getHistory (date) {
+  let indexArr = getIndexFile()
+  let file = History + date
+  if (indexArr.indexOf(date) === -1) {
+    return {}
+  }
+  if (fs.existsSync(path.join(historyDir, file))) {
+    let content = fs.readFileSync(file)
+    return JSON.parse(content)
+  } else {
+    if (fs.existsSync(path.join(historyDir, file))) {
+      let content = fs.readFileSync(file)
+      return JSON.parse(content)
+    } else {
+      return {}
+    }
+  }
 }
 
 export function getHistory () {
-  console.log('')
+  // let indexArr = getIndexFile()
+  return _getHistory(moment().format('YYYY-MM'))
 }
